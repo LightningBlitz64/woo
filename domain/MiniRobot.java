c744fce4ec0790534078ba27fa8075f191ac5b8c
LightningBlitz64 <omarhass641@gmail.com>
2024-08-04 19:39:17 -0500

    Create MiniRobot.java

---
+++ domain/MiniRobot.java
@@ -0,0 +1,15 @@
+package domain;
+
+public class MiniRobot {
+
+    public void initialize() {
+        System.out.println("Initializing");
+    }
+    public void move() {
+        System.out.println("Moving");
+    }
+    public void rotate() {
+        System.out.println("Rotating");
+    }
+
+}
