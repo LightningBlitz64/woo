c0d2c00fe6a0845e5bdc589d5661084eb89d5d4a
LightningBlitz64 <omarhass641@gmail.com>
2024-09-22 14:35:39 -0500

    Update Servo.java

--- Servo.java
+++ Servo.java
@@ -1,52 +1,141 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+import com.qualcomm.robotcore.hardware.Servo;
 
-@TeleOp(name="Omar")
-public class Omar extends LinearOpMode {
-    Servo hookL;
-    Servo hookR;
-    DcMotor backLeftDrive;
-    DcMotor backRightDrive;
-    DcMotor frontLeftDrive;
-    DcMotor frontRightDrive;
-    DcMotor wrist;
-    DcMotor leftShoulder;
-    DcMotor rightShoulder;
-    BNO055IMU imu;
 
-@Override
+
+
+/*
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When a selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this OpMode to the Driver Station OpMode list
+ */
+
+@TeleOp(name = "OmarArcadeMode", group = "LinearOpMode")
+
+public class OmarArcadeMode extends LinearOpMode {
+    private DcMotor frontleftDrive = null;
+    private DcMotor backleftDrive = null;
+    private DcMotor frontrightDrive = null;
+    private DcMotor backrightDrive = null;
+    private Servo hookR = null;
+
+
+    @Override
     public void runOpMode() {
-      hookL = hardwareMap.get(Servo.class, "hookL");
-      hookR = hardwareMap.get(Servo.class, "hookR");
-        
-      // Put initialization blocks here
-      waitForStart();
-      // Put run blocks here
-      while (opModeIsActive()) {
-          if (gamepad1.y){
+        frontleftDrive = hardwareMap.get(DcMotor.class, "fl");
+        frontrightDrive = hardwareMap.get(DcMotor.class, "fr");
+        backleftDrive = hardwareMap.get(DcMotor.class, "bl");
+        backrightDrive = hardwareMap.get(DcMotor.class, "br");
+        hookR = hardwareMap.get(Servo.class, "flclimb");
+        frontleftDrive.setDirection(DcMotor.Direction.REVERSE);
+        frontrightDrive.setDirection(DcMotor.Direction.FORWARD);
+        backleftDrive.setDirection(DcMotor.Direction.REVERSE);
+        backrightDrive.setDirection(DcMotor.Direction.FORWARD);
+
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+        // Wait for the game to start (driver presses PLAY)
+        double drive, turn, strafe;
+        double flpower, frpower, blpower, brpower;
+        waitForStart();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+            
+            drive =-gamepad1.left_stick_y;
+            strafe =gamepad1.left_stick_x;
+            turn =gamepad1.right_stick_x;
+            
+            frpower = drive - turn - strafe;
+            brpower
+            
+            
+            
+            
+            
+            = drive - turn + strafe;
+            flpower = drive + turn + strafe;
+            blpower = drive + turn - strafe;
+            
+            double maxPower = Math.max(+-       Math.abs(frpower),Math.max(Math.abs(brpower),Math.max(Math.abs(flpower), Math.abs(blpower))));
+            if(maxPower > 1){
+                frpower /= maxPower;
+                brpower /= maxPower;
+                flpower /= maxPower;
+                blpower /= maxPower;
+            }
+            
+            frontrightDrive.setPower(frpower);
+            backrightDrive.setPower(brpower);
+            frontleftDrive.setPower(flpower);
+            backleftDrive.setPower(blpower);
+            
+            if (gamepad1.y){
               
-              hookL.setPosition(0);
+              hookR.setPosition(0);
               
           } 
           
           else if(gamepad1.x){
-              hookL.setPosition(0.7);
+              hookR.setPosition(0.7);
               
           }
           
           else if(gamepad1.a){
               
-              hookL.setPosition(1);
+              hookR.setPosition(1);
               
           }
-          
-       
+            
+            
+            telemetry.addData("Status", "Running");
+            telemetry.update();
 
-      }
+        }
     }
-    
 }
+
+
